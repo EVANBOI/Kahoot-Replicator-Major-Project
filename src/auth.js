cd6440891ad345e5a29369b191fd54bda3bbe00e
Zhihao Cao <z5424549@ad.unsw.edu.au>
2024-06-04 11:39:24 +1000

    add: adminUserDetailsUpdate

---
+++ src/auth.js
@@ -0,0 +1,11 @@
+function adminUserDetailsUpdate (authUserId) {
+    return { user:
+        {
+          userId: 1,
+          name: 'Hayden Smith',
+          email: 'hayden.smith@unsw.edu.au',
+          numSuccessfulLogins: 3,
+          numFailedPasswordsSinceLastLogin: 1,
+        }
+    }
+}

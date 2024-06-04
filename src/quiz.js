c60b3d11a06d73669449035f798f783327f4357d
Jessie Zhang <z5422458@ad.unsw.edu.au>
2024-06-04 19:54:53 +1000

    Added adminQuizDescriptionUpdate function

--- src/quiz.js
+++ src/quiz.js
@@ -5,5 +5,26 @@ function adminQuizList ( authUserId ) {
             name: 'My Quiz',
             }
         ]
-    }   
+    }
+}
+
+function adminQuizRemove (authUserId, quizId) {
+    return {
+        
+    }
+}
+
+
+function adminQuizInfo (authUserId, quizId) {
+    return {
+        quizId: 1,
+        name: 'My Quiz',
+        timeCreated: 1683125870,
+        timeLastEdited: 1683125871,
+        description: 'This is my quiz',
+    }
+}
+
+function adminQuizDescriptionUpdate (authUserId, quizId, description) {
+    
 }

077354c75fd7ff934ee2036ced550daae1818ae6
Zhihao Cao <z5424549@ad.unsw.edu.au>
2024-06-13 12:21:39 +1000

    Add: return val test for clear

--- src/tests/clear.test.js
+++ src/tests/clear.test.js
@@ -1,7 +1,11 @@
-import { describe } from "yargs"
 import { clear } from "../other.js"
 import { data } from "../dataStore.js"
 
-describe('Function clear tests', () => {
 
-})
+describe('Function clear tests', () => {
+    test('Test - return value check', () => {
+        console.log("Running clear function test");
+        expect(clear()).toEqual({});
+    });
+});
+

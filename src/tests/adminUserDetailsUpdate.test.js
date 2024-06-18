d9cdc52749b15e7cf86a1b19d84a589287f05497
Zhihao Cao <z5424549@ad.unsw.edu.au>
2024-06-18 16:52:38 +1000

    Add: all error tests

--- src/tests/adminUserDetailsUpdate.test.js
+++ src/tests/adminUserDetailsUpdate.test.js
@@ -38,33 +38,85 @@ describe('error tests', () => {
         VALID_INPUTS_2.LASTNAME
     );
 
+    test('AuthUserId is not a valid user', ()=> {
+        const INVALID_ID = VALID_ID_1.authUserId + VALID_ID_2.authUserId + 100;
+        expect(adminUserDetailsUpdate(
+            INVALID_ID, 
+            VALID_INPUTS_1.EMAIL, 
+            VALID_INPUTS_1.FIRSTNAME, 
+            VALID_INPUTS_1.LASTNAME)
+        ).toStrictEqual(ERROR);
+    });
     
-    const INVALID_ID = VALID_ID_1.authUserId + VALID_ID_2.authUserId;
+    test('Email is currently used by another user', () => {
+        expect(adminUserDetailsUpdate(
+            VALID_ID_1, 
+            VALID_INPUTS_2.EMAIL, 
+            VALID_INPUTS_1.FIRSTNAME, 
+            VALID_INPUTS_1.LASTNAME)
+        ).toStrictEqual(ERROR);
+    });
 
-    test.each([
-        { 
-            error: 'AuthUserId is not a valid user',
-            authUserId: INVALID_ID, 
-            email: VALID_INPUTS_1.EMAIL, 
-            nameFirst: VALID_INPUTS_1.FIRSTNAME, 
-            nameLast: VALID_INPUTS_1.LASTNAME
-        }, 
-        {
-            error: 'Email is currently used by another user',
-            authUserId: VALID_ID_1, 
-            email: VALID_INPUTS_2.EMAIL, 
-            nameFirst: VALID_INPUTS_1.FIRSTNAME, 
-            nameLast: VALID_INPUTS_1.LASTNAME
-        }, 
-        {
-            error: 'Email is currently used by another user',
-            authUserId: VALID_ID_1, 
-            email: VALID_INPUTS_2.EMAIL, 
-            nameFirst: VALID_INPUTS_1.FIRSTNAME, 
-            nameLast: VALID_INPUTS_1.LASTNAME
-        }
-    ]) ('error: $error', ({authUserId, email, nameFirst, nameLast}) => {
-        expect(adminUserDetailsUpdate(authUserId, email, nameFirst, nameLast)).toStrictEqual(ERROR);
+    test('Email is invalid', () => {
+        const INVALID_EMAIL = 'INVALIDEMAIL';
+        expect(adminUserDetailsUpdate(
+            VALID_ID_1, 
+            INVALID_EMAIL, 
+            VALID_INPUTS_1.FIRSTNAME, 
+            VALID_INPUTS_1.LASTNAME)
+        ).toStrictEqual(ERROR);
     })
 
+    test('First name has invalid letter', () => {
+        const INVALID_FIRSTNAME = '!Invalid*Name'
+        expect(adminUserDetailsUpdate(
+            VALID_ID_1, 
+            VALID_INPUTS_1.EMAIL, 
+            INVALID_FIRSTNAME, 
+            VALID_INPUTS_1.LASTNAME)
+        ).toStrictEqual(ERROR);
+    })
+
+    test('Last name has invalid letter', () => {
+        const INVALID_LASTNAME = '!Invalid*Name'
+        expect(adminUserDetailsUpdate(
+            VALID_ID_1, 
+            VALID_INPUTS_1.EMAIL, 
+            VALID_INPUTS_1.FIRSTNAME, 
+            INVALID_LASTNAME)
+        ).toStrictEqual(ERROR);
+    })
+
+    test.each([
+        {
+            error: 'First name too long',
+            authUserId: VALID_ID_1, 
+            email: VALID_INPUTS_1.EMAIL, 
+            nameFirst: 'Toooooooooooooooooooooooooolong', 
+            nameLast: VALID_INPUTS_1.LASTNAME
+        },
+        {
+            error: 'First name too short',
+            authUserId: VALID_ID_1, 
+            email: VALID_INPUTS_1.EMAIL, 
+            nameFirst: 's', 
+            nameLast: VALID_INPUTS_1.LASTNAME
+        },
+        {
+            error: 'Last name too long',
+            authUserId: VALID_ID_1, 
+            email: VALID_INPUTS_1.EMAIL, 
+            nameFirst: VALID_INPUTS_1.FIRSTNAME, 
+            nameLast: 's'
+        },
+        {
+            error: 'Last name too short',
+            authUserId: VALID_ID_1, 
+            email: VALID_INPUTS_1.EMAIL, 
+            nameFirst: VALID_INPUTS_1.FIRSTNAME, 
+            nameLast: 'Toooooooooooooooooooooooooolong'
+        },
+    ]) ('$error', ({authUserId, email, nameFirst, nameLast}) => {
+        expect(adminUserDetailsUpdate(authUserId, email, nameFirst, nameLast)).toStrictEqual(ERROR);
+    })
 })

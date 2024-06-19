4fa02f3df2cb21baf7ca34938315e097397df955
Evan Xiong <z5421149@ad.unsw.edu.au>
2024-06-19 16:07:45 +1000

    Edit: adminAuthLogin; added setData onto the feature such that we can save the changes

--- src/auth.js
+++ src/auth.js
@@ -118,11 +118,13 @@ export function adminAuthLogin (email, password) {
         return { error: 'email address does not exist'};
     } else if (!correctPassword) {
         validEmail.numFailedPasswordsSinceLastLogin += 1;
+        setData(dataBase);
         return { error: 'password is not correct for the given email'};
     }
 
     correctPassword.numFailedPasswordsSinceLastLogin = 0;
     correctPassword.numSuccessfulLogins += 1;
+    setData(dataBase);
 
     return {
         authUserId: correctPassword.userId

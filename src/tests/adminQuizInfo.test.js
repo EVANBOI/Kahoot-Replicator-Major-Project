d9cfe60a5fdadfed3c1629e2e29d831813cd0f0b
Zhihao Cao <z5424549@ad.unsw.edu.au>
2024-06-19 20:31:58 +1000

    Add: all tests for adminQuizInfo function

--- src/tests/adminQuizInfo.test.js
+++ src/tests/adminQuizInfo.test.js
@@ -10,7 +10,8 @@ const VALID_USER = {
 }
 
 const VALID_QUIZ = {
-
+    NAME: 'ValidQuizName', 
+    DESCRIPTION: 'ValidDescription'
 }
 
 let VALID_USER_ID;
@@ -38,11 +39,44 @@ describe('error tests', () => {
         expec(adminQuizInfo(VALID_USER_ID + 1, VALID_QUIZ_ID)).toStrictEqual(ERROR);
     });
 
-    test('Quiz ID does not refer to a valid quiz.', () => {
+    test('QuizId is not a valid quiz.', () => {
         expec(adminQuizInfo(VALID_USER_ID, VALID_QUIZ_ID + 1)).toStrictEqual(ERROR);
     });
 
-    test.todo('Quiz ID does not refer to a valid quiz.', () => {
-       
+    test('Visitor is not creator', () => {
+        VALID_QUIZ_ID = adminQuizCreate(VALID_USER_ID, VALID_QUIZ.NAME, VALID_QUIZ.DESCRIPTION).quizId;
+        const ANOTHETR_USER_ID = adminAuthRegister(
+            'validAnotherEmail@gmail.com', 
+            VALID_USER.PASSWORD, 
+            VALID_USER.FIRSTNAME, 
+            VALID_USER.LASTNAME
+        ).authUserId;
+        expect(adminQuizInfo(ANOTHETR_USER_ID, VALID_QUIZ_ID)).toStrictEqual(ERROR);
+    });
+})
+
+describe('success tests', () => {
+    beforeEach(() => {
+        VALID_USER_ID = adminAuthRegister(
+            VALID_USER.EMAIL, 
+            VALID_USER.PASSWORD, 
+            VALID_USER.FIRSTNAME, 
+            VALID_USER.LASTNAME
+        ).authUserId;
+        VALID_QUIZ_ID = adminQuizCreate(
+            VALID_USER_ID, 
+            VALID_QUIZ.NAME, 
+            VALID_QUIZ.DESCRIPTION
+        ).quizId;
+    });
+
+    test('correct return value', () => {
+        expect(adminQuizInfo(VALID_USER_ID, VALID_QUIZ_ID)).toStrictEqual({
+            quizId: VALID_QUIZ_ID,
+            name: VALID_QUIZ.NAME,
+            timeCreated: expect.any(Number),
+            timeLastEdited: expect.any(Number),
+            description: VALID_QUIZ.DESCRIPTION,
+        });
     });
 })

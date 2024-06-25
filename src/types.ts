1f1b9fc079cca0cd101e7b54781f026b57e31224
Zhihao Cao <z5424549@ad.unsw.edu.au>
2024-06-25 14:05:14 +1000

    Edit: some typos in types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,13 @@
 export type EmptyObject = { };
 
-export type ErrorMessage = { };
+export type ErrorMessage = { error: string };
 
 export type ClearResult = EmptyObject;
 
 export type UserUpdateResult = EmptyObject | ErrorMessage;
 
 export type QuizInfoResult = ErrorMessage | {
-    quizId: string,
+    quizId: number,
     name: string,
     timeCreated: number,
     timeLastEdited: number,

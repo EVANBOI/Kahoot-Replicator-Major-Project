fa4b53dfb3496e16e5e6c1b6f173cc47a1a98a24
Jessie Zhang <z5422458@ad.unsw.edu.au>
2024-07-07 17:08:09 +1000

    Edit: fixed lint issues

--- src/wrappers.ts
+++ src/wrappers.ts
@@ -79,40 +79,39 @@ Did you res.json(undefined)?`,
   return bodyObject;
 };
 
-
-//============================================================================//
+//= ===========================================================================//
 // Wrappers
 
 export const adminAuthRegister = (
-    email: string, 
-    nameFirst: string, 
-    nameLast: string, 
-    password: string) => {
-    return requestHelper('POST', '/v1/admin/auth/register', 
-        { email, nameFirst, nameLast, password });
+  email: string,
+  nameFirst: string,
+  nameLast: string,
+  password: string) => {
+  return requestHelper('POST', '/v1/admin/auth/register',
+    { email, nameFirst, nameLast, password });
 };
-  
+
 export const adminQuizList = (sessionId: string) => {
-    return requestHelper('GET', '/v1/admin/quiz/list', { sessionId })
-}
+  return requestHelper('GET', '/v1/admin/quiz/list', { sessionId });
+};
 
 export const adminQuizDescriptionUpdate = (quizId: number) => {
-    return requestHelper('PUT', `/v1/admin/quia/${quizId}/description`, {})
-}
+  return requestHelper('PUT', `/v1/admin/quia/${quizId}/description`, {});
+};
 
 export const adminUserDetailsUpdate = (
   sessionId: string,
   email: string,
   nameFirst: string,
   nameLast: string) => {
-    return requestHelper('PUT', '/v1/admin/user/details', 
-        { sessionId, email, nameFirst, nameLast });
+  return requestHelper('PUT', '/v1/admin/user/details',
+    { sessionId, email, nameFirst, nameLast });
 };
 
 export const adminQuizInfo = (sessionId: string, quizId: number) => {
   return requestHelper('GET', `/v1/admin/quiz/${quizId}`, { sessionId });
-}
+};
 
 export const clear = () => {
   return requestHelper('DELETE', '/v1/clear', {});
-}
+};

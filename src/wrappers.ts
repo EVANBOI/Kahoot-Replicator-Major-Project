30acf32e3970a99eae993d5c2937fc0a321d32f7
Zhihao Cao <z5424549@ad.unsw.edu.au>
2024-07-23 12:30:37 +1000

    Edit: fixed moveInfo object wrapped twice as object in wrapper and serverroute

--- src/wrappers.ts
+++ src/wrappers.ts
@@ -214,5 +214,5 @@ export const adminQuizQuestionMove = (
   quizid: number,
   questionid: number,
   moveInfo: PositionWithTokenObj) => {
-  return requestHelper('PUT', `/v1/admin/quiz/${quizid}/question/${questionid}/move`, { moveInfo });
+  return requestHelper('PUT', `/v1/admin/quiz/${quizid}/question/${questionid}/move`, moveInfo);
 };

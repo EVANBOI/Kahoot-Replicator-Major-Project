9c8b0c6200f0833035f743a257ec5f7711b371e8
Zhihao Cao <z5424549@ad.unsw.edu.au>
2024-07-30 17:57:46 +1000

    Add: tests for the adminQuizSessionView

--- src/tests/adminQuizSessionView.test.ts
+++ src/tests/adminQuizSessionView.test.ts
@@ -1,5 +1,6 @@
 import { 
-    ERROR401, ERROR403, VALID_USER_REGISTER_INPUTS_1, VALID_QUIZ_CREATE_INPUTS_1, validQuestion1V2
+    ERROR401, ERROR403, VALID_USER_REGISTER_INPUTS_1, VALID_QUIZ_CREATE_INPUTS_1, 
+    validQuestion1V2, VALID_USER_REGISTER_INPUTS_2, VALID_QUIZ_CREATE_INPUTS_2
 } from '../testConstants';
 import {
   clear,
@@ -8,12 +9,13 @@ import {
   adminQuizSessionUpdate,
   adminQuizSessionStart,
   adminCreateQuizQuestionV2,
+  adminQuizSessionView
 } from '../wrappers';
 
 let token1: string;
-let sessionId1: number;
-let quizId1: number;
-let questionId1: number;
+let sessionId1: number, sessionId2: number;
+let quizId1: number, quizId2: number;
+let questionId1: number, questionId2: number;
 beforeEach(() => {
     clear();
     token1 = adminAuthRegister(
@@ -29,21 +31,84 @@ beforeEach(() => {
       ).jsonBody.quizId;
     questionId1 = adminCreateQuizQuestionV2(quizId1, token1, validQuestion1V2).jsonBody.questionId;
     sessionId1 = adminQuizSessionStart(quizId1, token1, 5).jsonBody.sessionId;
-  });
+});
 
 
 describe('GET /v1/admin/quiz/{quizid}/sessions', () => {
     describe('error cases', () => {
-        test.todo('Error 401: token is invalid');
-        test.todo('Error 401: token is empty');
-        test.todo('Error 403: quiz does not exist');
-        test.todo('Error 403: user is not owner of quiz');
+        test('Error 401: token is empty', () => {
+            expect(adminQuizSessionView('', quizId1)).toStrictEqual(ERROR401);
+        });
+        test('Error 401: token is invalid', () => {
+            expect(adminQuizSessionView(token1 + 1, quizId1)).toStrictEqual(ERROR401);
+        });
+        test('Error 403: quiz does not exist', () => {
+            expect(adminQuizSessionView(token1, quizId1 + 1)).toStrictEqual(ERROR403);
+        });
+        test('Error 403: user is not owner of quiz', () => {
+            const token2 = adminAuthRegister(
+                VALID_USER_REGISTER_INPUTS_2.EMAIL,
+                VALID_USER_REGISTER_INPUTS_2.PASSWORD,
+                VALID_USER_REGISTER_INPUTS_2.FIRSTNAME,
+                VALID_USER_REGISTER_INPUTS_2.LASTNAME
+            ).jsonBody.token;
+            expect(adminQuizSessionView(token2, quizId1)).toStrictEqual(ERROR403);
+        });
     });
 
     describe('success cases', () => {
-        test.todo('Successfully view session with only 1 active session existing');
-        test.todo('Successfully view session with only 1 inactive session existing');
-        test.todo('Successfully view session status with no sessions existing');
-        test.todo('Successfully view session status with both active and inactive sessions existing');
+        test('Successfully view session with only 1 active session existing', () => {
+            adminQuizSessionUpdate(quizId1, sessionId1, token1, "LOBBY");
+            expect(adminQuizSessionView(token1, quizId1)).toStrictEqual({
+                activeSessions: [sessionId1],
+                inactiveSessions: []
+            });
+        });
+        test('Successfully view session with only 1 inactive session existing', () => {
+            adminQuizSessionUpdate(quizId1, sessionId1, token1, "END");
+            expect(adminQuizSessionView(token1, quizId1)).toStrictEqual({
+                activeSessions: [],
+                inactiveSessions: [sessionId1]
+            });
+        });
+        test('Successfully view session with no sessions existing', () => {
+            quizId2 = adminQuizCreate(
+                token1,
+                VALID_QUIZ_CREATE_INPUTS_2.NAME,
+                VALID_QUIZ_CREATE_INPUTS_2.DESCRIPTION
+              ).jsonBody.quizId;
+            questionId2 = adminCreateQuizQuestionV2(quizId2, token1, validQuestion1V2).jsonBody.questionId;
+            expect(adminQuizSessionView(token1, quizId1)).toStrictEqual({
+                activeSessions: [],
+                inactiveSessions: []
+            });
+        });
+        test('Successfully view session with both active and inactive sessions existing', () => {
+            sessionId2 = adminQuizSessionStart(quizId1, token1, 5).jsonBody.sessionId;
+            adminQuizSessionUpdate(quizId1, sessionId1, token1, "LOBBY");
+            adminQuizSessionUpdate(quizId1, sessionId2, token1, "END");
+            expect(adminQuizSessionView(token1, quizId1)).toStrictEqual({
+                activeSessions: [sessionId1],
+                inactiveSessions: [sessionId2]
+            });
+        });
+        test('Successfully view session with multiple active sessions existing', () => {
+            sessionId2 = adminQuizSessionStart(quizId1, token1, 5).jsonBody.sessionId;
+            adminQuizSessionUpdate(quizId1, sessionId1, token1, "LOBBY");
+            adminQuizSessionUpdate(quizId1, sessionId2, token1, "LOBBY");
+            expect(adminQuizSessionView(token1, quizId1)).toStrictEqual({
+                activeSessions: [sessionId1, sessionId2],
+                inactiveSessions: []
+            });
+        });
+        test('Successfully view session with multiple inactive sessions existing', () => {
+            sessionId2 = adminQuizSessionStart(quizId1, token1, 5).jsonBody.sessionId;
+            adminQuizSessionUpdate(quizId1, sessionId1, token1, "END");
+            adminQuizSessionUpdate(quizId1, sessionId2, token1, "END");
+            expect(adminQuizSessionView(token1, quizId1)).toStrictEqual({
+                activeSessions: [],
+                inactiveSessions: [sessionId1, sessionId2]
+            });
+        });
     });
 })
